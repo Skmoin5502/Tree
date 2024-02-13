95668094c52fc90241622ef56c05598cefa9de06
Moin <skmoin5502@gmail.com>
2024-02-13 16:42:44 +0530

    first commit

---
+++ cin.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+	int data;
+	struct node *left, *right;
+};
+
+struct node *create(){
+	int x;
+	struct node *newnode;
+	newnode = new node;
+	cout << "Enter data(-1 for no node)";
+	cin >> x;
+	if (x==-1){
+		return 0;
+	}
+	newnode->data=x;
+	cout << "Enter left child of "<< x;
+	newnode->left=create();
+	cout << "Enter right child of "<< x;
+	newnode->right=create();
+	return newnode;
+}
+
+void preorder(struct node *r){
+	if(r==NULL){
+ 		cout <<"-";
+	}
+	else{
+ 		cout << r->data;
+ 		preorder(r->left);
+ 		preorder(r->right);
+	}
+}
+
+void Inorder(struct node *r){
+	if(r==NULL){
+ 		cout <<"-";
+	}
+	else{
+ 		Inorder (r->left);
+ 		cout << r->data;
+ 		Inorder (r->right);
+	}
+}
+
+void Postorder(struct node *r){
+	if(r==NULL){
+ 		cout <<"-";
+	}
+	else{
+ 		Postorder (r->left);
+ 		Postorder (r->right);
+ 		cout << r->data;
+	}
+}
+
+
+int main(){
+	struct node *root;
+	root= NULL;
+	root=create();
+	
+	cout << "Inorder traversal: ";
+	Inorder(root);
+	cout <<"\n";
+	
+	cout << "Preorder traversal: ";
+	preorder(root);
+	cout <<"\n";
+	
+	cout << "Postorder traversal: ";
+	Postorder(root);
+	
+}
